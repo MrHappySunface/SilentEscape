c920b6239c3691e2c59b8c1745e6b43a98d365dc
tejindra77 <tejindrakhatriofficial@gmail.com>
2025-03-05 11:28:23 -0500

    gameplay logic

--- SilentEscapeGame/Assets/Scripts/Gameplay/SonarSystem.cs
+++ SilentEscapeGame/Assets/Scripts/Gameplay/SonarSystem.cs
@@ -2,3 +2,26 @@
 
 using UnityEngine;  // Core Unity functions
 using System.Collections;  // Required for coroutines
+
+public class SonarSystem : MonoBehaviour
+{
+    public float sonarSpeed = 5f;
+    public float sonarRadius = 5f;
+
+    void Start()
+    {
+        StartCoroutine(ExpandSonar());
+    }
+
+    IEnumerator ExpandSonar()
+    {
+        float currentRadius = 0f;
+        while (currentRadius < sonarRadius)
+        {
+            currentRadius += sonarSpeed * Time.deltaTime;
+            transform.localScale = new Vector3(currentRadius, currentRadius, currentRadius);
+            yield return null;
+        }
+        Destroy(gameObject);
+    }
+}
--- SilentEscapeGame/Assets/Scripts/Gameplay/TrapSystem.cs
+++ SilentEscapeGame/Assets/Scripts/Gameplay/TrapSystem.cs
@@ -1,3 +1,35 @@
 // Traps, tripwires, glassess, etc..
 
 using UnityEngine;  // Core Unity functions
+
+public class TrapSystem : MonoBehaviour
+{
+    public enum TrapType { BearTrap, Tripwire, Hole }
+    public TrapType trapType;
+    public AudioSource trapSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (trapType == TrapType.BearTrap)
+            {
+                Debug.Log("Player Trapped!");
+                trapSound.Play();
+                StartCoroutine(TrapDuration(other.gameObject));
+            }
+            else if (trapType == TrapType.Hole)
+            {
+                Debug.Log("Player Fell!");
+                other.GetComponent<PlayerController>().enabled = false;
+            }
+        }
+    }
+
+    IEnumerator TrapDuration(GameObject player)
+    {
+        player.GetComponent<PlayerController>().speed = 0;
+        yield return new WaitForSeconds(5f);
+        player.GetComponent<PlayerController>().speed = 3.0f;
+    }
+}
