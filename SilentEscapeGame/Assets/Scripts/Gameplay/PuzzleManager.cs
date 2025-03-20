73b2faa1db8f9b04171b35aa9da79c6e4a1994d5
tejindra77 <tejindrakhatriofficial@gmail.com>
2025-03-20 16:34:46 -0400

    script change

--- SilentEscapeGame/Assets/Scripts/Gameplay/PuzzleManager.cs
+++ SilentEscapeGame/Assets/Scripts/Gameplay/PuzzleManager.cs
@@ -21,3 +21,30 @@ public class PuzzleManager : MonoBehaviour
 }
 
 */
+
+
+// Puzzle handling
+/*
+using UnityEngine;  // Core Unity functions
+
+public class PuzzleManager : MonoBehaviour
+{
+    public bool puzzleSolved = false;
+    public AudioSource puzzleCompleteSound;
+    public Light puzzleIndicator;
+
+    public void SolvePuzzle()
+    {
+        if (!puzzleSolved)
+        {
+            Debug.Log("Puzzle Solved!");
+            puzzleSolved = true;
+            puzzleCompleteSound.Play();
+            puzzleIndicator.color = Color.green;
+        }
+    }
+}
+
+*/
+
+
